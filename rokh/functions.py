91015e734e0e9d81da90293ed6b47df5be16a1ff
Sadra Sabouri <43045767+sadrasabouri@users.noreply.github.com>
2025-09-30 04:01:10 -0700

    Initial Structure Added (#1)
    * add : init file for the library added.
    
    * add : init file for events added.
    
    * add : function file added.
    
    * add : paramters added.
    
    * add : jalali calendar added.

---
+++ rokh/functions.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""Functions for the rokh package"""
+from typing import List, Dict, Tuple, Union
+from .events.jalali import EVENTS as JALALI_EVENTS
+from .params import Calendar
+
+
+def _handle_jalali_date(month: Union[str, int], day: Union[str, int]) -> Tuple[str, str]:
+    """
+    Ensure month and day are strings for Jalali calendar events lookup.
+    
+    :param month: The month in the Jalali calendar (1-12).
+    :param day: The day in the Jalali calendar (1-31).
+    :return: A tuple of (month, day) as strings.
+    """
+    return str(month), str(day)
+
+
+def _get_jalali_events(month: Union[str, int], day: Union[str, int]) -> List[Dict[str, str]]:
+    """
+    Retrieve Jalali calendar events for a specific month and day.
+    
+    :param month: The month in the Jalali calendar (1-12).
+    :param day: The day in the Jalali calendar (1-31).
+    """
+    month, day = _handle_jalali_date(month, day)
+    return JALALI_EVENTS.get(month, {}).get(day, [])
+
+
+CALENDAR_MAP = {
+    Calendar.JALALI: _get_jalali_events,
+}
+
+
+def get_events(
+        calendar: Calendar,
+        month: Union[str, int],
+        day: Union[str, int]) -> List[Dict[str, str]]:
+    """
+    Retrieve events for a specific month and day in the specified calendar.
+
+    :param calendar: The calendar type.
+    :param month: The month in the specified calendar.
+    :param day: The day in the specified calendar.
+    """
+    return CALENDAR_MAP[calendar](month, day)
---
+++ rokh/params.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Parameters for the rokh package"""
+from enum import Enum
+
+class Calendar(Enum):
+    """Enumeration for calendar types"""
+    JALALI = "jalali"
+
+    DEFAULT = JALALI
