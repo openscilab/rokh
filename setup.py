b540dfefbfe61267ad101c07fab4ab5e6f4ba67e
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-10-02 09:49:58 +0330

    Repo Structure (#3)
    * file : setup.py added
    
    * file : SECURITY.md added
    
    * file : requirements.txt and dev-requirements.txt added
    
    * doc : README.md updated
    
    * file : codecov.yml added
    
    * file : CHANGELOG.md added
    
    * file : autopep script added
    
    * file : AUTHORS.md aded
    
    * file : .coveragerc added
    
    * file : otherfiles folder added
    
    * file : .github folder added
    
    * fix : version_check.py updated
    
    * doc : CONTRIBUTING.md updated
    
    * doc : minor bug in RELEASE.md fixed
    
    * fix : ROKH_VERSION added to params.py

---
+++ setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Setup module."""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def get_requires() -> list:
+    """Read requirements.txt."""
+    requirements = open("requirements.txt", "r").read()
+    return list(filter(lambda x: x != "", requirements.split()))
+
+
+def read_description() -> str:
+    """Read README.md and CHANGELOG.md."""
+    try:
+        with open("README.md") as r:
+            description = "\n"
+            description += r.read()
+        with open("CHANGELOG.md") as c:
+            description += "\n"
+            description += c.read()
+        return description
+    except Exception:
+        return '''TODO'''
+
+
+setup(
+    name='rokh',
+    packages=[
+        'rokh', ],
+    version='0.1',
+    description='TODO',
+    long_description=read_description(),
+    long_description_content_type='text/markdown',
+    author='Rokh Development Team',
+    author_email='rokh@openscilab.com',
+    url='https://github.com/openscilab/rokh',
+    download_url='https://github.com/openscilab/rokh/tarball/v0.1',
+    keywords="events date date-system",
+    project_urls={
+            'Source': 'https://github.com/openscilab/rokh',
+    },
+    install_requires=get_requires(),
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Manufacturing',
+        'Topic :: Education',
+    ],
+    license='MIT',
+)
