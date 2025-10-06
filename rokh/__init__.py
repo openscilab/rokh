3b82ed3476db44ad30eead6339b78ad5caaeb2d4
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-10-06 19:14:24 +0330

    fix : __init__.py updated

--- rokh/__init__.py
+++ rokh/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """Initialize the rokh package"""
 
-from .functions import get_events, get_jalali_events
+from .functions import get_events
+from .functions import get_jalali_events
+from .functions import get_gregorian_events
+from .functions import get_hijri_events
 from .params import ROKH_VERSION, DateSystem
 
 __version__ = ROKH_VERSION
