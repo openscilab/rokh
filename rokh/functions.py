76500401d8b3adc01e5b6682c254b42978ad1668
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-10-06 17:19:53 +0330

    feat : convert functions added

--- rokh/functions.py
+++ rokh/functions.py
@@ -3,6 +3,47 @@
 from typing import List, Dict, Tuple, Union
 from .events.jalali import EVENTS as JALALI_EVENTS
 from .params import Calendar
+import jdatetime
+from hijridate
+
+
+def _convert_to_gregorian(input_date_system: DateSystem, year: int, month: int, day: int) -> Tuple[int, int, int]:
+    """
+    Convert from input date system to Gregorian.
+
+    :param input_date_system: input date system
+    :param year: year
+    :param month: month
+    :param day: day
+    """
+    if input_date_system == DateSystem.GREGORIAN:
+        return (year, month, day)
+    elif input_date_system == DateSystem.JALALI:
+        g = jdatetime.JalaliToGregorian(year, month, day)
+        return (g.gyear, g.gmonth, g.gday)
+    elif input_date_system == DateSystem.HIJRI:
+        g = hijridate.Hijri(year, month, day).to_gregorian()
+        return (g.year, g.month, g.day)
+
+
+def _convert_from_gregorian(target_date_system: DateSystem, year: int, month: int, day: int) -> Tuple[int, int, int]:
+    """
+    Convert from Gregorian to target date system.
+
+    :param target_date_system: target date system
+    :param year: year
+    :param month: month
+    :param day: day
+    """
+    if target_date_system == DateSystem.GREGORIAN:
+        return (year, month, day)
+    elif target_date_system == DateSystem.JALALI:
+        import jdatetime
+        j = jdatetime.GregorianToJalali(year, month, day)
+        return (j.jyear, j.jmonth, j.jday)
+    elif target_date_system == DateSystem.HIJRI:
+        h = hijridate.Gregorian(g_date.year, g_date.month, g_date.day).to_hijri()
+        return (h.year, h.month, h.day)
 
 
 def _handle_jalali_date(month: Union[str, int], day: Union[str, int]) -> Tuple[str, str]:
