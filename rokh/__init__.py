c394fcda6fd76fd92ffaecb2e9ba1e523351aab4
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-10-06 23:01:56 +0330

    Get Events (#7)
    * feat : convert functions added
    
    * fix : Calendar --> DateSystem
    
    * fix : setup.py updated
    
    * feat : get_gregorian_events and get_hijri_events functions updated
    
    * fix : __init__.py updated
    
    * fix : requirements updated
    
    * fix : requirements updated
    
    * doc : docstring updated

--- rokh/__init__.py
+++ rokh/__init__.py
@@ -2,6 +2,9 @@
 """Initialize the rokh package"""
 
 from .functions import get_events
-from .params import ROKH_VERSION, Calendar
+from .functions import get_jalali_events
+from .functions import get_gregorian_events
+from .functions import get_hijri_events
+from .params import ROKH_VERSION, DateSystem
 
 __version__ = ROKH_VERSION
--- rokh/functions.py
+++ rokh/functions.py
@@ -1,46 +1,118 @@
 # -*- coding: utf-8 -*-
 """Functions for the rokh package"""
-from typing import List, Dict, Tuple, Union
+from typing import List, Dict, Tuple, Union, Optional
 from .events.jalali import EVENTS as JALALI_EVENTS
-from .params import Calendar
+from .events.gregorian import EVENTS as GREGORIAN_EVENTS
+from .events.hijri import EVENTS as HIJRI_EVENTS
+from .params import DateSystem
+import datetime
+import jdatetime
+import hijridate
 
 
-def _handle_jalali_date(month: Union[str, int], day: Union[str, int]) -> Tuple[str, str]:
+def _convert_to_gregorian(input_date_system: DateSystem, day: int, month: int, year: int) -> Tuple[int, int, int]:
     """
-    Ensure month and day are strings for Jalali calendar events lookup.
+    Convert from input date system to Gregorian.
 
-    :param month: The month in the Jalali calendar (1-12).
-    :param day: The day in the Jalali calendar (1-31).
-    :return: A tuple of (month, day) as strings.
+    :param input_date_system: input date system
+    :param day: day in input date system
+    :param month: month in input date system
+    :param year: year in input date system
     """
-    return str(month), str(day)
+    if input_date_system == DateSystem.GREGORIAN:
+        return (day, month, year)
+    elif input_date_system == DateSystem.JALALI:
+        g = jdatetime.JalaliToGregorian(year, month, day)
+        return (g.gday, g.gmonth, g.gyear)
+    elif input_date_system == DateSystem.HIJRI:
+        g = hijridate.Hijri(year, month, day).to_gregorian()
+        return (g.day, g.month, g.year)
 
 
-def _get_jalali_events(month: Union[str, int], day: Union[str, int]) -> List[Dict[str, str]]:
+def _convert_from_gregorian(target_date_system: DateSystem,  day: int, month: int, year: int) -> Tuple[int, int, int]:
     """
-    Retrieve Jalali calendar events for a specific month and day.
+    Convert from Gregorian to target date system.
 
-    :param month: The month in the Jalali calendar (1-12).
-    :param day: The day in the Jalali calendar (1-31).
+    :param target_date_system: target date system
+    :param day: day in Gregorian date system
+    :param month: month in Gregorian date system
+    :param year: year in Gregorian date system
     """
-    month, day = _handle_jalali_date(month, day)
-    return JALALI_EVENTS.get(month, {}).get(day, [])
+    if target_date_system == DateSystem.GREGORIAN:
+        return (day, month, year)
+    elif target_date_system == DateSystem.JALALI:
+        j = jdatetime.GregorianToJalali(year, month, day)
+        return (j.jday, j.jmonth, j.jyear)
+    elif target_date_system == DateSystem.HIJRI:
+        h = hijridate.Gregorian(year, month, day).to_hijri()
+        return (h.day, h.month, h.year)
 
 
-CALENDAR_MAP = {
-    Calendar.JALALI: _get_jalali_events,
-}
+def get_jalali_events(day: int, month: int, year: int= None) -> List[Dict[str, str]]:
+    """
+    Retrieve Jalali events for a specific date.
+
+    :param day: day in Jalali date system
+    :param month: month in Jalali date system
+    :param year: year in Jalali date system
+    """
+    return JALALI_EVENTS.get(str(month), {}).get(str(day), [])
+
+
+def get_gregorian_events(day: int, month: int, year: int= None) -> List[Dict[str, str]]:
+    """
+    Retrieve Gregorian events for a specific date.
+
+    :param day: day in Gregorian date system
+    :param month: month in Gregorian date system
+    :param year: year in Gregorian date system
+    """
+    return GREGORIAN_EVENTS.get(str(month), {}).get(str(day), [])
+
+
+def get_hijri_events(day: int, month: int, year: int= None) -> List[Dict[str, str]]:
+    """
+    Retrieve Hijri events for a specific date.
+
+    :param day: day in Hijri date system
+    :param month: month in Hijri date system
+    :param year: year in Hijri date system
+    """
+    return HIJRI_EVENTS.get(str(month), {}).get(str(day), [])
 
 
 def get_events(
-        calendar: Calendar,
-        month: Union[str, int],
-        day: Union[str, int]) -> List[Dict[str, str]]:
+    day: int,
+    month: int,
+    year: Optional[int] = None,
+    input_date_system: DateSystem = DateSystem.JALALI,
+    event_date_system: Optional[DateSystem] = None,
+) -> Dict[str, List[Dict[str, str]]]:
     """
-    Retrieve events for a specific month and day in the specified calendar.
+    Retrieve events for a specific day, month and year in the specified date system.
 
-    :param calendar: The calendar type.
-    :param month: The month in the specified calendar.
-    :param day: The day in the specified calendar.
+    :param day: day in input date system
+    :param month: month in input date system
+    :param year: year in input date system
+    :param input_date_system: input date system
+    :param event_date_system: event date system
     """
-    return CALENDAR_MAP[calendar](month, day)
+    if year is None:
+        year = datetime.now().year
+    gregorian_date = _convert_to_gregorian(input_date_system, day, month, year)
+    jalali_date = _convert_from_gregorian(DateSystem.JALALI, *gregorian_date)
+    hijri_date = _convert_from_gregorian(DateSystem.HIJRI, *gregorian_date)
+    result = {"events": []}
+
+    if event_date_system is None:
+        result["events"].extend(get_jalali_events(*jalali_date))
+        result["events"].extend(get_gregorian_events(*gregorian_date))
+        result["events"].extend(get_hijri_events(*hijri_date))
+    else:
+        if event_date_system == DateSystem.JALALI:
+            result["events"] = get_jalali_events(*jalali_date)
+        elif event_date_system == DateSystem.GREGORIAN:
+            result["events"] = get_gregorian_events(*gregorian_date)
+        elif event_date_system == DateSystem.HIJRI:
+            result["events"] = get_hijri_events(*hijri_date)
+    return result
