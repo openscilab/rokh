0437fcad91558405c4dae3da3394af5d97d7b953
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-10-07 11:17:51 +0330

    fix : __init__.py updated

--- rokh/__init__.py
+++ rokh/__init__.py
@@ -2,6 +2,7 @@
 """Initialize the rokh package"""
 
 from .functions import get_events
+from .errors import RokhValidationError
 from .params import ROKH_VERSION, DateSystem
 
 __version__ = ROKH_VERSION
