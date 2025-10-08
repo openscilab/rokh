e25ee771f52c5069b6cddeb4d6264e60a323fd89
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-10-08 18:56:17 +0330

    fix : input_date_system and event_date_system added to get_events function output

--- rokh/functions.py
+++ rokh/functions.py
@@ -145,13 +145,14 @@ def get_events(
     gregorian_date = _convert_to_gregorian(input_date_system, day, month, year)
     jalali_date = _convert_from_gregorian(DateSystem.JALALI, *gregorian_date)
     hijri_date = _convert_from_gregorian(DateSystem.HIJRI, *gregorian_date)
-    result = {"events": dict(), "is_holiday": False}
+    result = {"events": dict(), "is_holiday": False, "input_date_system": input_date_system.value, "event_date_system": "all"}
 
     if event_date_system is None:
         result["events"]["jalali"] = _get_jalali_events(*jalali_date)
         result["events"]["gregorian"] = _get_gregorian_events(*gregorian_date)
         result["events"]["hijri"] = _get_hijri_events(*hijri_date)
     else:
+        result["event_date_system"] = event_date_system.value
         if event_date_system == DateSystem.JALALI:
             result["events"]["jalali"] = _get_jalali_events(*jalali_date)
         elif event_date_system == DateSystem.GREGORIAN:
