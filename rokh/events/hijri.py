d7bf33b7a645c53e728c257a12b157bb95d6543a
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-10-17 16:01:43 +0330

    fix : Hijri events updated

--- rokh/events/hijri.py
+++ rokh/events/hijri.py
@@ -8,13 +8,15 @@ EVENTS = {
           '12': [{'description': 'شهادت  امام زین العابدین علیه السلام', 'is_holiday': False}]},
     # صَفَر
     "2": {'20': [{'description': 'اربعین حسینی', 'is_holiday': True}],
-          '28': [{'description': 'رحلت  رسول اکرم؛شهادت امام حسن مجتبی علیه السلام', 'is_holiday': True}],
+          '28': [{'description': 'رحلت  رسول اکرم', 'is_holiday': True},
+                 {'description': 'شهادت امام حسن مجتبی علیه السلام', 'is_holiday': True}],
           '30': [{'description': 'شهادت امام رضا علیه السلام', 'is_holiday': True}]},
     # رَبِيع ٱلْأَوَّل
     "3": {'1': [{'description': 'هجرت پیامبر اکرم از مکه به مدینه', 'is_holiday': False}],
           '8': [{'description': 'شهادت امام حسن عسکری علیه السلام', 'is_holiday': True}],
           '12': [{'description': 'میلاد رسول اکرم به روایت اهل سنت', 'is_holiday': False}],
-          '17': [{'description': 'میلاد رسول اکرم و امام جعفر صادق علیه السلام', 'is_holiday': True}]},
+          '17': [{'description': 'میلاد رسول اکرم', 'is_holiday': True},
+                 {'description': 'ولادت امام جعفر صادق علیه السلام', 'is_holiday': True}]},
     # رَبِيع ٱلثَّانِي
     "4": {'8': [{'description': 'ولادت امام حسن عسکری علیه السلام', 'is_holiday': False}],
           '10': [{'description': 'وفات حضرت معصومه سلام الله علیها', 'is_holiday': False}]},
@@ -25,7 +27,7 @@ EVENTS = {
           '20': [{'description': 'ولادت حضرت فاطمه زهرا سلام الله علیها و روز مادر', 'is_holiday': False}]},
     # رَجَب
     "7": {'1': [{'description': 'ولادت  امام محمد باقر علیه السلام', 'is_holiday': False}],
-          '3': [{'description': 'شهادت امام علی النقی الهادی علیه السلام', 'is_holiday': False}],
+          '3': [{'description': 'شهادت امام علی النقی علیه السلام', 'is_holiday': False}],
           '10': [{'description': 'ولادت امام محمد تقی علیه السلام', 'is_holiday': False}],
           '13': [{'description': 'ولادت امام علی علیه السلام و روز پدر', 'is_holiday': True}],
           '15': [{'description': 'وفات حضرت زینب سلام الله علیها', 'is_holiday': False}],
@@ -49,12 +51,13 @@ EVENTS = {
            '25': [{'description': 'شهادت امام جعفر صادق علیه السلام', 'is_holiday': True}]},
     # ذُو ٱلْقَعْدَة
     "11": {'1': [{'description': 'ولادت حضرت معصومه سلام الله علیها و روز دختران', 'is_holiday': False}],
-           '11': [{'description': 'ولادت امام  رضا علیه السلام', 'is_holiday': False}]},
+           '11': [{'description': 'ولادت امام  رضا علیه السلام', 'is_holiday': False}],
+           '30': [{'description': 'شهادت امام محمد تقی علیه السلام', 'is_holiday': False}]},
     # ذُو ٱلْحِجَّة
     "12": {'7': [{'description': 'شهادت امام محمد باقر علیه السلام', 'is_holiday': False}],
            '9': [{'description': 'روز عرفه', 'is_holiday': False}],
            '10': [{'description': 'عید سعید قربان', 'is_holiday': True}],
-           '15': [{'description': 'ولادت امام علی النقی الهادی علیه السلام', 'is_holiday': False}],
+           '15': [{'description': 'ولادت امام علی النقی علیه السلام', 'is_holiday': False}],
            '18': [{'description': 'عید سعید غدیر خم', 'is_holiday': True}],
            '20': [{'description': 'ولادت امام موسی کاظم علیه السلام', 'is_holiday': False}]}
 }
