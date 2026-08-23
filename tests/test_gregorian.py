c274ea3ded67a354769d025a51a6470b45ae9ec5
Sadra Sabouri <sabouri.sadra@gmail.com>
2026-08-22 23:44:25 -0700

    tests : tests added.

--- tests/test_gregorian.py
+++ tests/test_gregorian.py
@@ -1,4 +1,4 @@
-from rokh import get_events, get_today_events, is_holiday, DateSystem
+from rokh import get_events, get_today_events, is_holiday, is_today_holiday, DateSystem
 from rokh.events.jalali import EVENTS as JALALI_EVENTS
 from rokh.events.gregorian import EVENTS as GREGORIAN_EVENTS
 from rokh.events.hijri import EVENTS as HIJRI_EVENTS
@@ -250,3 +250,14 @@ def test_is_holiday_gregorian():
                         input_date_system=DateSystem.GREGORIAN,
                         event_date_system=DateSystem.HIJRI)
     assert result == False
+
+
+def test_is_today_holiday_gregorian():
+    today = datetime.datetime.now()
+    month = today.month
+    day = today.day
+    year = today.year
+    result = is_today_holiday(event_date_system=DateSystem.GREGORIAN)
+    assert result == is_holiday(day, month, year,
+                                input_date_system=DateSystem.GREGORIAN,
+                                event_date_system=DateSystem.GREGORIAN)
