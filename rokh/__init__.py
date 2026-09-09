09908cae290eef6a2662a7b4bec6db28d1150086
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-09-09 17:28:09 +0330

    fix : update __init__.py

--- rokh/__init__.py
+++ rokh/__init__.py
@@ -8,4 +8,11 @@ from .params import ROKH_VERSION, DateSystem
 __version__ = ROKH_VERSION
 
 
-__all__ = ["get_events", "get_today_events", "is_holiday", "is_today_holiday", "RokhValidationError", "DateSystem"]
+__all__ = [
+    "get_events",
+    "get_today_events",
+    "is_holiday",
+    "is_today_holiday",
+    "RokhValidationError",
+    "DateSystem"
+    ]
