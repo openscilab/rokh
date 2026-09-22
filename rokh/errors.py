4d52a17d55c93087d710a48f17eb61c285d4247d
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-09-22 21:46:43 +0330

    feat : add RokhError class

--- rokh/errors.py
+++ rokh/errors.py
@@ -2,6 +2,10 @@
 """Rokh errors."""
 
 
+class RokhError(Exception):
+    """Base exception for all Rokh errors."""
+
+
 class RokhValidationError(ValueError):
     """Base class for validation errors in Rokh."""
 
